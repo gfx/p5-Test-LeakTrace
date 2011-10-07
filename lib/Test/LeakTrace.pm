a1bdcc93fca79260aa021e3185108e868fdd9987
Fuji Goro <fuji.goro@dena.jp>
2011-10-07 02:24:24 -0700

    Checking in changes prior to tagging of version 0.14.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 7e23a37..881d236 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Test::LeakTrace
    
    +0.14 2011-10-07 02:22:11
    +    - Fix Test::Valgrind failures (thanks to @shohex)
    +
     0.13 Sun Aug  1 15:17:23 2010
         - Fix tests for older perls

--- lib/Test/LeakTrace.pm
+++ lib/Test/LeakTrace.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 use XSLoader;
 XSLoader::load(__PACKAGE__, $VERSION);
@@ -130,7 +130,7 @@ Test::LeakTrace - Traces memory leaks
 
 =head1 VERSION
 
-This document describes Test::LeakTrace version 0.13.
+This document describes Test::LeakTrace version 0.14.
 
 =head1 SYNOPSIS
 
